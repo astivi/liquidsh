07fd1286c860eebe093a9289d69d2d9273e70c02
Álvaro Stivi <alvaro.stivi@quintoandar.com.br>
2016-11-02 11:33:55 -0200

    Creates dir for output

--- liquid.rb
+++ liquid.rb
@@ -5,7 +5,9 @@ unless ARGV.length != 2 then
     filename = ARGV[0]
     @template = Liquid::Template.parse(File.read(filename))
     result = @template.render(JSON.parse(File.read(ARGV[1])))
-    output = "#{filename}.html"
+    dir = "#{File.dirname(File.absolute_path(filename))}/output"
+    Dir.mkdir(dir) unless Dir.exist?(dir)
+    output = "#{dir}/#{filename}.html"
     f = File.new(output, 'w')
     puts "Writing result to #{output}"
     f.write(result)
