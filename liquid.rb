c86ced2c74919bdc8a62eae1c2ca7ff864aedd0e
Álvaro Stivi <alvaro.stivi@quintoandar.com.br>
2016-11-03 19:41:07 -0200

    Fixes basename of file

--- liquid.rb
+++ liquid.rb
@@ -7,7 +7,7 @@ unless ARGV.length != 2 then
     result = @template.render(JSON.parse(File.read(ARGV[1])))
     dir = "#{File.dirname(File.absolute_path(filename))}/output"
     Dir.mkdir(dir) unless Dir.exist?(dir)
-    output = "#{dir}/#{filename}.html"
+    output = "#{dir}/#{File.basename(filename, '.*')}.html"
     f = File.new(output, 'w')
     puts "Writing result to #{output}"
     f.write(result)
